d41ab62eb319e2bfd8fd2f18ce7d6dfc34eca75d
Tiago Loriato <tloriatosimoes@gmail.com>
2019-10-10 18:18:26 +0100

    feat: models, migrations and database

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,13 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Tiago',
-        'title': 'Hello World',
-        'content': 'Post text Post text',
-        'date_posted': 'October 09, 2019',
-    },
-    {
-        'author': 'Tiago',
-        'title': 'Bye world',
-        'content': 'Post text Post text',
-        'date_posted': 'October 10, 2019',
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About Us'})
+    
